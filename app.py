bf43ab1ba6434658686bbd47adf93f409e43e8c7
José Miguel Calderon Frutos <josemiguelcalderonfrutos@gmail.com>
2020-04-25 13:48:05 +0200

    Finacilazcion de pagina detail

--- app.py
+++ app.py
@@ -4,13 +4,21 @@ import json
 with open('books.json') as file:
     libros=json.load(file)
 
-prueba=["hola","bueno","dias"]
-
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template("Inicio.html",datos=libros,hola=prueba)
+    return render_template("Inicio.html",datos=libros)
 
+@app.route('/libro/<isbn>')
+def detail(isbn):
+    for libro in libros:
+        if isbn == libro.get("isbn"):
+            return render_template("Detail.html",libro=libro)
+    abort(404)
+
+@app.route('/categoria/<categoria>')
+def catgoria(categoria):
+    
 
 app.run(debug=True)
